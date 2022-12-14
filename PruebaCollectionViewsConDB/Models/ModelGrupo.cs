3a298f2705f59432b1af25cb3f3d09d1bd505346
jorge <jorge.ricard@utp.edu.co>
2022-12-14 16:04:59 -0500

    Agregar archivos de proyecto.

---
+++ PruebaCollectionViewsConDB/Models/ModelGrupo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PruebaCollectionViewsConDB.Models
+{
+    public class ModelGrupo
+    {
+        public string codigo { get; set; }
+        public string nombre { get; set; }
+        public string tipo { get; set; }
+    }
+}
---
+++ PruebaCollectionViewsConDB/Services/ServicesGrupo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using PruebaCollectionViewsConDB.Models;
+
+namespace PruebaCollectionViewsConDB.Services
+{
+    public class ServicesGrupo
+    {
+        public ObservableCollection<ModelGrupo> listGrupos;
+        public ServicesGrupo() { }
+
+        public static ObservableCollection<ModelGrupo> obtenerLista()
+        {
+
+            return new ObservableCollection<ModelGrupo>
+            {
+                new ModelGrupo
+                {
+                    codigo="100",
+                    nombre="Cascos",
+                    tipo="C"
+                },
+                new ModelGrupo
+                {
+                    codigo="200",
+                    nombre="Tarros",
+                    tipo="T"
+                },
+                new ModelGrupo
+                {
+                    codigo="300",
+                    nombre="Tornillos",
+                    tipo="To"
+                }
+            };
+        }
+        
+    }
+}
